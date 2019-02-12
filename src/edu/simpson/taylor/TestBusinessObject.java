a92df8418509f316c6dc6e91c1b0cc51ccf3a323
MATTSLENOVOFLEX\Matt <matt.taylor@my.simpson.edu>
2019-02-12 12:52:45 -0600

    Assignment 4 Example Code

---
+++ src/edu/simpson/taylor/TestBusinessObject.java
@@ -0,0 +1,8 @@
+package edu.simpson.taylor;
+
+public class TestBusinessObject
+{
+    private String fieldname;
+    public String getFieldName() { return fieldname; }
+    public void setFieldName(String fieldname) { this.fieldname = fieldname; }
+}
